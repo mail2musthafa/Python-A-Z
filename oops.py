8f17ea98da57eddf0deec5f1c0c9832db33e1708
mail2musthafa <mail2musthafa.md@gmail.com>
2024-02-25 14:05:58 +0530

    oops updated
    oops some examples are updared

---
+++ oops.py
@@ -0,0 +1,162 @@
+#this session we will cover the oops concepts in python 
+#class is blue print or template or model or disign or  plan
+#object --a physical existance or  instance of a class 
+# reference variable :refernce variableis refers to perform required operation on objcect
+#perobject an number of variable may be there 
+# if refernce vriable is not prsent of any object then we cont use the object for  dffernet operation the no use of creation object 
+# class---<object<---refernce variable 
+#class 
+    # properties or attributes or Variables 
+    # behaviour  by using methods 
+    # methods --actions 
+
+# class className:
+    #'''Documentation string represets descrotion of object'''
+    # variables==properties or attributes
+    # methods or attribies or Behaviours
+
+class Student:
+    '''this sample demo while cration class '''
+    #variables 
+    #methods 
+Student()  
+print(Student.__doc__)
+help(Student)
+#Withi in the pytho  class there typs of variable 
+# variable(3)
+# instant variable   --object level variable 
+# static varaible ---class level variable 
+# local variable  --methods level vaiable
+
+# metods(3)
+# instant methods 
+# class methods
+# static methods 
+class Student:
+    '''class devlaped for demo '''
+    def __init__(self):
+        self.name = 'md' 
+        self.rollnumber = 100
+        self.marks = 99
+    def talk(self):
+        print('hello i am ',self.name)
+        print('my marks are ',self.marks)  
+object = Student()
+print(object.name) 
+print(object.marks)     
+print(object.rollnumber)
+object.talk()
+#---------------------------------------------
+class Student:
+    def __init__(self,name,rollnum,marks):
+        self.nmae = name
+        self.rollnum = rollnum
+        self.marks = marks
+    def chat(self):
+        print('my name is :',self.name)
+        print('my rollnumber is :'self.rollnum)
+        print('my marks are :',self.marks)
+object1 = Student('md',100,80)
+object1.chat()
+#------------------------------------------------
+class Dog:
+    #class attributes 
+    attr1 = 'mammal'
+    #Instance attribite
+    def __init__(self,name):
+        self.name = name
+#object instantiation 
+german_shappord = Dog('German Shappord')   
+print(german_shappord.name)
+print(tommy.name)
+#print()
+#-------------------------------------
+class Dog:
+ 
+    # class attribute
+    attr1 = "mammal"
+ 
+    # Instance attribute
+    def __init__(self, name):
+        self.name = name
+ 
+# Driver code
+# Object instantiation
+Rodger = Dog("Rodger")
+Tommy = Dog("Tommy")
+ 
+# Accessing class attributes
+print("Rodger is a {}".format(Rodger.__class__.attr1))
+print("Tommy is also a {}".format(Tommy.__class__.attr1))
+ 
+# Accessing instance attributes
+print("My name is {}".format(Rodger.name))
+print("My name is {}".format(Tommy.name))
+#----------------------------------------------------
+'''creating the class and objects with methds'''
+class Dog:
+    #class attribute 
+    attr1= 'mammal'
+    #instatace attrubutes
+    def __init__(self,name):
+        self.name = name
+    def speak(self):
+        print('my name is {}'.format(self.name))
+Rodger = Dog('Rodger')  
+Tommy = Dog('Tommy')     
+print(Tommy)
+print(Tommy.name)
+#------------------------------------------------
+ 
+# parent class
+class Person(object):
+ 
+    # __init__ is known as the constructor
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+ 
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+         
+    def details(self):
+        print("My name is {}".format(self.name))
+        print("IdNumber: {}".format(self.idnumber))
+     
+# child class
+class Employee(Person):
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+ 
+        # invoking the __init__ of the parent class
+        Person.__init__(self, name, idnumber)
+         
+    def details(self):
+        print("My name is {}".format(self.name))
+        print("IdNumber: {}".format(self.idnumber))
+        print("Post: {}".format(self.post))
+ 
+ 
+# creation of an object variable or an instance
+a = Employee('Rahul', 886012, 200000, "Intern")
+ 
+# calling a function of the class Person using
+# its instance
+a.display()
+a.details()
+
+
+
+#369380139351  
+    
+    
+
+
+
+          
+
+     
+
+
