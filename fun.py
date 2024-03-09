ea5fb15483461df5661729b71190397024dc14ce
mail2musthafa <mail2musthafa.md@gmail.com>
2024-03-09 13:06:59 +0530

    commit
    commit updated

--- fun.py
+++ fun.py
@@ -5,5 +5,27 @@ print('welcome the world of alainans ')
 
 
 def fun1():
-    print('welcome')
+    pass
 fun1()    
+
+#function with  parameters 
+def add(num1:int,num2:int) ->int:
+    result =num1+num2
+    return result
+num1,num2 = 23,20
+ans = add(num1,num2)
+print(f"the addition of num1 and num2 is {num1},{num2} is {ans}")
+#some more functions 
+def is_prime(n):
+    if n in [2, 3]:
+        return True
+    if (n == 1) or (n % 2 == 0):
+        return False
+    r = 3
+    while r * r <= n:
+        if n % r == 0:
+            return False
+        r += 2
+    return True
+print(is_prime(78), is_prime(79))
+    
